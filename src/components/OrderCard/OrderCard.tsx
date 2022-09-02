39b5c77b41ce0f53b9ac87d6f4befced12042608
sergera <sergera88@gmail.com>
2022-09-02 20:39:07 -0300

    New Order Card Style
    - Changed style of OrderCard component & Page component so that it fits one
    order per row

--- src/components/OrderCard/OrderCard.tsx
+++ src/components/OrderCard/OrderCard.tsx
@@ -17,22 +17,30 @@ export function OrderCard({
 			<div style={{gridTemplateColumns:"1fr"}} className="order-card__header">
 				{unconfirmed &&
 				<div className={"order-card__unconfirmed order-card__status"}>
+					<div className="order-card__status-value">
 						<p>unconfirmed</p>
+					</div>
 				</div>
 				}
 				{inProgress &&
 				<div className={"order-card__in-progress order-card__status"}>
+					<div className="order-card__status-value">
 						<p>in progress</p>
+					</div>
 				</div>
 				}
 				{ready &&
 				<div className={"order-card__ready order-card__status"}>
+					<div className="order-card__status-value">
 						<p>ready</p>
+					</div>
 				</div>
 				}
 				{inTransit &&
 				<div className={"order-card__in-transit order-card__status"}>
+					<div className="order-card__status-value">
 						<p>in transit</p>
+					</div>
 				</div>
 				}
 				{delivered &&
@@ -41,17 +49,24 @@ export function OrderCard({
 				</div>
 				}
 			</div>
-			<div className="order-card__body">
 			<div className="order-card__id">
+				<div className="order-card__id-value">
 					{"#" + order.orderId}
+				</div>
 			</div>
-			<div className="order-card__created">
-				{"Created: " + order.date + " " + order.time}
+			<div className="order-card__price">
+				<div className="order-card__price-value">
+					{"$"+order.price}
+				</div>
 			</div>
-				<div className="order-card__price">
-					<div className="order-card__price-value">
-						{"$"+order.price}
-					</div>
+			<div className="order-card__date">
+				<div className="order-card__date-value">
+					{order.date}
+				</div>
+			</div>
+			<div className="order-card__time">
+				<div className="order-card__time-value">
+					{order.time}
 				</div>
 			</div>
 		</div>
